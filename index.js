7a68afde56dc5ef3038b4278a3198ec02f235ee9
Julian M. Qapo <julian.qapo@gmail.com>
2018-12-05 14:50:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solutions in this file
+
+let list = ['Ada', 'Brendan', 'Ali']
+
+function printBadges(list){
+for (i=0; i<list.length; i++)
+console.log(`Welcome ${list[i]}! You are employee #${i+1}.`)
+
+}
+
+
