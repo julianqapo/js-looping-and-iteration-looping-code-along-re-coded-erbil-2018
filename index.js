6723d6919468aeb141f21ac17e61595ec3f7df33
Julian M. Qapo <julian.qapo@gmail.com>
2018-12-08 19:37:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+let arr = []
+
+function printBadges(arr){
+for (i=0; i<arr.length; i++){
+console.log(`Welcome ${arr[i]}! You are employee #${i+1}.`)
+arr = arr[i] + arr
+}
+
+}
+
+
+
+function tailsNeverFails(){
+
+}
